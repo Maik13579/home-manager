bd03ce60a9635bf51d447b6f70da62f53bafbea8
Maik Knof <knofm@hs-weingarten.de>
2025-05-02 19:32:55 +0200

    make terminal rainbow

--- programs/programs.nix
+++ programs/programs.nix
@@ -4,5 +4,6 @@
     bash = (import ./bash.nix { inherit pkgs; });
     dircolors = (import ./dircolors.nix { inherit pkgs; });
     terminator = (import ./terminator.nix { inherit pkgs; });
-    home-manager = (import ./home-manager.nix { inherit pkgs; });
+    home-manager.enable = true;
+    starship = (import ./starship.nix { inherit pkgs; });
 }
