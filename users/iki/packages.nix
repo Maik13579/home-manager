3e362395c3b88f515eabfc679927dc350e952d49
Maik Knof <knofm@hs-weingarten.de>
2025-05-04 09:11:46 +0200

    remove some packages

--- users/iki/packages.nix
+++ users/iki/packages.nix
@@ -1,19 +1,16 @@
 { pkgs, nixvim_pkg, ... }:
 with pkgs;
 [
+  #editor
   nixvim_pkg
-  alejandra # nix format
-  nixd # lsp
-
   lazygit
   xclip
-
-  nerd-fonts.jetbrains-mono
-  neofetch
-  nnn # terminal file manager
-
   clang
   clang-tools
+
+  nerd-fonts.jetbrains-mono
+
+  neofetch
 
   # archives
   zip
@@ -36,22 +33,14 @@ with pkgs;
   # it provides the command `nom` works just like `nix`
   # with more details log output
   nix-output-monitor
+  alejandra # nix format
+  nixd # lsp
 
   glow # markdown previewer in terminal
 
   btop # replacement of htop/nmon
-  iotop # io monitoring
-  iftop # network monitoring
-
-  # system call monitoring
-  strace # system call monitoring
-  ltrace # library call monitoring
-  lsof # list open files
 
   # system tools
-  sysstat
-  lm_sensors # for `sensors` command
-  ethtool
   pciutils # lspci
   usbutils # lsusb
 ]
