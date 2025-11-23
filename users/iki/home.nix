6f28d3a1356b72a52e63c89dfefe6dcb6984ea1f
Maik Knof <knofm@hs-weingarten.de>
2025-11-23 14:11:15 +0100

    remove stuff

--- users/iki/home.nix
+++ users/iki/home.nix
@@ -17,7 +17,6 @@
   # Files to place under $HOME
   home.file = {
     ".local/bin/repo2promt.sh".source = ../../scripts/repo2promt.sh;
-    ".config/bash/clone.bash".source = ../../scripts/clone.bash;
     ".config/bash/drun.bash".source = ../../scripts/drun.bash;
     ".dotfiles/.bashrc".source = ../../dotfiles/bashrc;
   };
